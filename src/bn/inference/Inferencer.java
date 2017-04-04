9899f40336b44eef87abab129afdef34c3b92543
schlayer <qwertbochaffer@gmail.com>
2017-04-04 18:19:39 -0400

    Needed for Testing

--- src/bn/inference/Inferencer.java
+++ src/bn/inference/Inferencer.java
@@ -27,40 +27,6 @@ public interface Inferencer {
 	}
 	
 
-	/**
-	function ENUMERATION-ASK(X, e, bn) returns a distribution over X
-		inputs: X, the query variable
-				e, observed values for variables E
-				bn, a Bayes net with variables {X} u  E u  Y // Y = hidden variables 
-		Q(X)<a distribution over X, initially empty
-		for each value xi of X do
-			Q(xi)<ENUMERATE-ALL(bn.VARS, exi )
-				where exi is e extended with X = xi
-		return NORMALIZE(Q(X))
-	 */ 
-	
-	public static Distribution exactEnumerationAsk(BayesianNetwork bn, RandomVariable X, Assignment a) {
-		return null;
-		
-	}
-	
-	/*
-	function ENUMERATE-ALL(vars, e) returns a real number
-		if EMPTY?(vars) then return 1.0
-		Y <FIRST(vars)
-		if Y has value y in e
-			then return P(y | parents(Y )) × ENUMERATE-ALL(REST(vars), e)
-			else return sum y P(y | parents(Y )) × ENUMERATE-ALL(REST(vars), ey)
-				where ey is e extended with Y = y
-	 */
-
-	
-	public static double exactEnumerateAll(RandomVariable X, Assignment a) {
-		
-		return 0;
-	}
-	
-	
 	
 
 }
